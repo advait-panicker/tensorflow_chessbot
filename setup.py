ad806a3ac971c544ecf0f8a461815dd357882547
advait-panicker <advait.panicker@yahoo.com>
2024-07-18 14:29:27 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='repository',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # dependencies
+    ],
+)

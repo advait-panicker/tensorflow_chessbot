246de46fcda2326ec4732f24802fd8fa8342f521
advait-panicker <advait.panicker@yahoo.com>
2024-07-18 14:34:37 -0400

    Updated package name and added dependencies

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='repository',
+    name='tensorflow_chessbot',  # Replace with your project name
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        # dependencies
+        'beautifulsoup4>=4.6.3,<5',
+        'lxml>=4.2.4,<5',
+        'Pillow>=5.2.0,<6',
+        'tensorflow>=1.5.0,<2',
+        'requests>=2.21.0,<3',
     ],
 )
